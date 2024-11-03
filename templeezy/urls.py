362e8e1d0272494636d057febdb7f8c31d9b46e7
Afam Olie <aolie94@gmail.com>
2024-11-02 22:05:03 -0700

    Initialized swagger endpoints in URLs
    - Updated readme to show how to shape your environment variable file
    - Added initial code for swagger documentation

--- templeezy/urls.py
+++ templeezy/urls.py
@@ -14,9 +14,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from django.contrib import admin
 from django.urls import path
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Templeezy API",
+        default_version="v1",
+        description="API for managing, previewing and saving templates.",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@templeezy.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
